7bd0ceec7353cc134c68262a8c3e7bac5873a7a6
leshka <adorogov@ooobit.com>
2019-06-05 14:12:51 +0500

    updated package

--- PhoenixService.Data/Resolvers/AppointmentsResolver.cs
+++ PhoenixService.Data/Resolvers/AppointmentsResolver.cs
@@ -1,14 +1,22 @@
 ﻿using PhoenixService.Data.Interfaces.Resolvers;
 using PhoenixService.Domain;
 using System;
+using NCore;
 
 namespace PhoenixService.Data.Resolvers
 {
     public class AppointmentsResolver : IAppointmentsResolver
     {
+        private readonly IFoxDbInteractor dbInteractor;
+
+        public AppointmentsResolver(IFoxDbInteractor dbInteractor)
+        {
+            this.dbInteractor = dbInteractor;
+        }
+
         public Appointment[] GetNearestByRequestId(string requestId)
         {
-            return Array.Empty<Appointment>();
+            dbInteractor.InitializeConnection()
         }
     }
 }
