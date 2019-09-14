970a91945902b5e8d9328eb3de3e3fa7f2b3ebc0
leshka <adorogov@ooobit.com>
2019-09-14 14:42:43 +0500

    another Api project

---
+++ PhoenixService.Web.ScheduleApi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PhoenixService.Web.ScheduleApi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
